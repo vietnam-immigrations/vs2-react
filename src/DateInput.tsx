5829c22a59d09b632b44ab079a5562917ec0fae9
Nam Truong Le <lenamtruong@gmail.com>
2023-09-02 10:50:14 +0200

    feature: add DateInput (#4)
    Co-authored-by: Nam Truong Le <nam.truong.le@sap.com>

---
+++ src/DateInput.tsx
@@ -0,0 +1,29 @@
+import { isValidDateString } from "./validator";
+import TextField from "@mui/material/TextField";
+import React from "react";
+
+export interface DateInputProps {
+  onChange: (value: string) => void;
+  value: string;
+  label: string;
+  className?: string;
+  required?: boolean;
+}
+
+export default function DateInput({ value, onChange, label, className, required }: DateInputProps) {
+  const valid = value === "" || isValidDateString(value);
+  const helperText = valid ? null : "Date format DD/MM/YYYY";
+  return (
+    <TextField
+      className={className}
+      error={!valid}
+      onChange={(event) => onChange(event.target.value)}
+      required={required}
+      size="small"
+      fullWidth
+      label={`${label} (DD/MM/YYYY)`}
+      value={value}
+      helperText={helperText}
+    />
+  );
+}
