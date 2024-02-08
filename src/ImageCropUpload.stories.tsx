bb302e4c22ca4645fb2f5929d2c8687eb56bc9ef
Nam Truong Le <lenamtruong@gmail.com>
2024-02-08 05:24:50 +0100

    fix: "react-image-crop": "^11.0.5" (#187)

--- src/ImageCropUpload.stories.tsx
+++ src/ImageCropUpload.stories.tsx
@@ -13,10 +13,20 @@ export default {
 
 export const Default: StoryFn<ImageCropUploadProps> = (args) => {
   const resetFileRef = React.useRef<(() => void) | null>(null);
+  const resetFileRef2 = React.useRef<(() => void) | null>(null);
 
   const handleCropComplete = (croppedFile: File | null) => {
     console.log("Cropped File:", croppedFile);
   };
 
-  return <ImageCropUpload {...args} onCropComplete={handleCropComplete} resetFileRef={resetFileRef} />;
+  const handleCropComplete2 = (croppedFile: File | null) => {
+    console.log("Cropped File:", croppedFile);
+  };
+
+  return (
+    <>
+      <ImageCropUpload {...args} onCropComplete={handleCropComplete} resetFileRef={resetFileRef} />
+      <ImageCropUpload {...args} onCropComplete={handleCropComplete2} resetFileRef={resetFileRef2} />
+    </>
+  );
 };
